6d6ecb0b046c55e8b1076fbf4a392dd156852a20
Hridya Premkumar <hridyapremkumar@Hridyas-MacBook-Air.local>
2025-09-28 08:35:24 +0530

    Frontend updates

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("68178f8d352cd7f6")
+/******/ 		__webpack_require__.h = () => ("aa31acc96a8a6aca")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b85dfda4a19f8ed6"; }
+/******/ 		__webpack_require__.h = function() { return "0dc3a1eb0be93211"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
